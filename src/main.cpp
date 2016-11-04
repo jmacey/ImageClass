183b9179d5499403c4f9f9a8b5a45e681323082b
jmacey <jmacey@bournemouth.ac.uk>
2016-11-04 11:15:46 +0000

    animate frames

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include "Image.h"
+#include <random>
+
+int main()
+{
+  constexpr size_t width=200;
+  constexpr size_t height=200;
+  std::cout<<"testing image\n";
+  Image test(width,height);
+
+  for(int frame=0; frame<width; ++frame )
+  {
+    test.clearScreen(255,255,255);
+    for(size_t x=0; x<frame; ++x)
+    {
+      for(size_t y=80; y<120; ++y)
+        test.setPixel(x,y,255,0,0);
+    }
+
+    char name[50];
+    sprintf(name,"frame.%04d.png",frame);
+    test.save(name);
+  }
+
+
+  return EXIT_SUCCESS;
+}
+
+
+
+
+
